a4b3bad33e1456a9aa99e906af98fbff41f1ae2c
Vdtrung_ <hellovdtvip@gmail.com>
2021-08-04 00:38:13 +0700

    update

--- preview-mpp-java-server/src/main/java/dev/webServer/controller/FilesController.java
+++ preview-mpp-java-server/src/main/java/dev/webServer/controller/FilesController.java
@@ -17,6 +17,7 @@ import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
 
 import java.io.*;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -90,83 +91,24 @@ public class FilesController {
                 .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"index.html\"").body(file);
     }
 
+    @GetMapping("/api/mppConverter")
+    public ResponseEntity<Resource> mppConverterApi(@RequestParam("file") MultipartFile file,
+                                                    @RequestParam("api-key") String apiKey, String fileFormat) {
+        ResponseEntity<ResponseMessage> messageEntity = null;
+        try {
+            messageEntity = fileUpload(file, apiKey);
+            Map<String, String> payload = new HashMap<>();
+            String fileGuid = messageEntity.getBody().getMessage();
+            payload.put("fileGuid", fileGuid);
+            payload.put("format", fileFormat);
+            messageEntity = convertFile(payload);
 
-
-
-    @GetMapping("/files")
-    public ResponseEntity<List<FileInfo>> getListFiles() {
-        List<FileInfo> fileInfos = storageService.loadAll().map(path -> {
-            String filename = path.getFileName().toString();
-            String url = MvcUriComponentsBuilder
-                    .fromMethodName(FilesController.class, "getFile", filename).build().toString();
-            String ganttImage = url + "/gantt-chart-image";
-            String json = url + "/json";
-
-            return new FileInfo(filename, url, ganttImage, json);
-        }).collect(Collectors.toList());
-
-        return ResponseEntity.status(HttpStatus.OK).body(fileInfos);
-    }
-
-    @GetMapping("/files/{filename:.+}")
-    public ResponseEntity<FileInfo> getFile(@PathVariable String filename) throws IOException {
-            Resource file = storageService.load(filename);
-
-            String url = MvcUriComponentsBuilder
-                    .fromMethodName(FilesController.class, "getFile", file.getFilename()).build().toString();
-            String ganttImage = url + "/gantt-chart-image";
-            String json = url + "/json";
-
-            FileInfo fileInfo = new FileInfo(filename, url, ganttImage, json);
-            return ResponseEntity.status(HttpStatus.OK).body(fileInfo);
-    }
-
-    @DeleteMapping("files/delete/{filename:.+}")
-    public ResponseEntity<ResponseMessage> deleteFile(@PathVariable String filename) {
-        String message;
-        if (storageService.delete(filename)) {
-            message = filename + " deleted! ";
-        } else {
-            message = "Could not delete this file !";
+            System.out.println(messageEntity);
+            return messageEntity.getStatusCode().equals(HttpStatus.OK) ? downloadFile(fileGuid) : null;
+        } catch(Exception e) {
+            System.out.println(messageEntity);
+            return null;
         }
-
-        return ResponseEntity.status(HttpStatus.OK).body(new ResponseMessage(message));
     }
-
-
-    @GetMapping("/files/json/{filename:.+}")
-    public ResponseEntity<Project> getJsonFile(@PathVariable String filename) {
-        Resource file = storageService.load(filename);
-        Project jsonProject = convertService.toJsonProject(file.getFilename());
-
-        if (jsonProject != null) {
-            return ResponseEntity.status(HttpStatus.OK)
-                    .body(jsonProject);
-        }
-
-        return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED)
-                .body(new Project("Could not read file: " + file.getFilename()));
-
-    }
-
-    @GetMapping(
-            value = "/files/gantt-chart-image/{filename:.+}",
-            produces = MediaType.IMAGE_JPEG_VALUE
-    )
-    public @ResponseBody byte[] getGanttImage(@PathVariable String filename) throws IOException {
-        Resource file = storageService.load(filename);
-        Project jsonProject = convertService.toJsonProject(file.getFilename());
-
-        if (jsonProject != null) {
-            convertService.ganttToImage(jsonProject);
-
-            Resource image = dataStorageService.load("gantt.png");
-            InputStream in = image.getInputStream();
-            return IOUtils.toByteArray(in);
-        }
-
-        return null;
-    }
-
 }
 
